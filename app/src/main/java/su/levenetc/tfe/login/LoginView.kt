afb5840769cfa4e897b910b4a1bd52c576ca3e22
Eugene Levenetc <eugene.levenetc@themobilecompany.com>
2018-01-14 04:30:50 +0100

    Update login view

--- app/src/main/java/su/levenetc/tfe/login/LoginView.kt
+++ app/src/main/java/su/levenetc/tfe/login/LoginView.kt
@@ -7,6 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import su.levenetc.tfe.R
 import su.levenetc.tfe.routing.Router
 
@@ -19,24 +23,55 @@ class LoginView : Fragment() {
         fun newInstance(router: Router): LoginView {
             val result = LoginView()
             result.router = router
-            result.retainInstance = true
             return result
         }
     }
 
     private lateinit var btnLogin: Button
     private lateinit var editName: EditText
+    private lateinit var textStatus: TextView
     private lateinit var router: Router
 
-    private var viewMode: LoginViewModel = LoginViewModel()
+    private var stateSub: Disposable? = null
+
+    private var viewModel: LoginViewModel = LoginViewModel()
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val result = inflater?.inflate(R.layout.view_login, container, false)
 
         btnLogin = result?.findViewById(R.id.btn_login)!!
         editName = result.findViewById(R.id.edit_name)
+        textStatus = result.findViewById(R.id.text_status)
 
         return result
     }
 
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        stateSub = viewModel.stateObservable()
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeOn(Schedulers.io())
+                .subscribe(this::handleState)
+    }
+
+    override fun onDestroyView() {
+        stateSub?.dispose()
+        super.onDestroyView()
+    }
+
+    private fun handleState(state: LoginState) {
+        if (state == LoginState.ERROR) {
+            textStatus.text = "Error"
+        } else {
+            textStatus.text = ""
+
+            if (state == LoginState.IDLE) {
+
+            } else if (state == LoginState.LOGGING_IN) {
+                textStatus.text = "Logging in..."
+            } else if (state == LoginState.SUCCESS) {
+                textStatus.text = "Logged in successfully"
+            }
+        }
+    }
 }
--- app/src/main/java/su/levenetc/tfe/login/LoginViewModel.kt
+++ app/src/main/java/su/levenetc/tfe/login/LoginViewModel.kt
@@ -1,6 +1,19 @@
 package su.levenetc.tfe.login
 
+import io.reactivex.Observable
+
 /**
  * Created by eugene.levenetc on 01/12/2017.
  */
-class LoginViewModel
+class LoginViewModel {
+
+
+    fun stateObservable(): Observable<LoginState> {
+
+        return Observable.just(LoginState.IDLE)
+    }
+
+    fun login(name: String) {
+
+    }
+}
